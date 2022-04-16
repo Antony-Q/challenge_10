24ae9942da36696dec49510b00e9cdebf6ff80f1
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-04-16 11:37:05 -0500

    didnt  save previous edit

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const render = require("./lib/htmlRender");
-
 const Team = require('./lib/Team');
 
 new Team().createTeam();
