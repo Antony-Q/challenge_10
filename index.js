f72154192a2b146c5f77b3a3cefd1559d899c9a0
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-05-01 15:12:41 -0500

    added new team file

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 const Team = require('./lib/Team');
 
 new Team().createTeam();
+
+function Team(name, id, email, office, role) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.office = office;
+    this.role = role;
+  }
+  
+  const createManager = (manager) => {
+    const { name, id, email, office, role } = manager;
+};
+  
+  console.log(Team.make);  
